f2a8f29216ac4c5886d39f6431d64c1e98520d4a
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-03-22 20:52:31 +0530

    custom api response and error handling, utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import 'dotenv/config'
 import mongoose from "mongoose";
 import connectDB from "./db/mongoDB.js";
+import {app} from './app.js'
 
 connectDB()
+.then( ()=> {
+
+    app.on("error", (error)=>{
+        console.log('ERRR:',error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at Port: ${process.env.PORT}`);        
+    })
+    
+})
+.catch((error)=>{
+    console.log("MONGO db connection failed !!!", error);
+    
+})
 
 
 
